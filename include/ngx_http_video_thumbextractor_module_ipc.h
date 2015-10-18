5727cef73b1038b5299eaf9047cb16419fede9ed
Wandenberg <wandenberg@gmail.com>
2015-10-18 21:29:14 -0200

    add a configuration to set how many processes will be created per worker to extract the thumbs

---
+++ include/ngx_http_video_thumbextractor_module_ipc.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2011 Wandenberg Peixoto <wandenberg@gmail.com>
+ *
+ * This file is part of Nginx Video Thumb Extractor Module.
+ *
+ * Nginx Video Thumb Extractor Module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nginx Video Thumb Extractor Module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nginx Video Thumb Extractor Module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * ngx_http_video_thumbextractor_module_utils.h
+ *
+ * Created:  Nov 22, 2011
+ * Author:   Wandenberg Peixoto <wandenberg@gmail.com>
+ *
+ */
+#ifndef NGX_HTTP_VIDEO_THUMBEXTRACTOR_MODULE_IPC_H_
+#define NGX_HTTP_VIDEO_THUMBEXTRACTOR_MODULE_IPC_H_
+
+ngx_int_t       ngx_http_video_thumbextractor_module_child_pids[NGX_MAX_PROCESSES];
+
+#endif /* NGX_HTTP_VIDEO_THUMBEXTRACTOR_MODULE_IPC_H_ */
